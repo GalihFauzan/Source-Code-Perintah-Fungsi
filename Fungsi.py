d967b796d277ab4c1dd3cc3e691a4c233f2ecc28
GalihFauzan <74187545+GalihFauzan@users.noreply.github.com>
2020-11-13 19:13:52 +0700

    Update Fungsi.py

--- Fungsi.py
+++ Fungsi.py
@@ -1,10 +1,8 @@
-# REKURSIF
-def rekursif(angka):
-    if angka > 0:
-        print(angka)
-        angka = angka - 1
-        rekursif(angka)
-    else:
-        print(angka)
-masukkan = int(input("Masukkan Angka: "))
-rekursif(masukkan)
+# MEMBUAT FUNGSI
+def salam():
+    print('Hallo Semuanya')
+    
+## Pemanggilan Fungsi
+salam()
+salam()
+salam()
